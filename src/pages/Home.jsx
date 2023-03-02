ef121a3c487b7ef283a494584b53b4588cac8c63
Todd <thickam@pdx.edu>
2023-03-01 22:05:02 -0800

    finalizing position styles for all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 const Home = () => {
   return (
     <>
-      {/* <span class="introduction"> */}
+    <article>
       <div className="container profile-body justify-content-center">
-        {/* <article className="introduction"> */}
         <h1 className="">Todd Hickam</h1>
         <img
           class="profile-image my-2"
@@ -11,17 +10,17 @@ const Home = () => {
           alt="headshot of Todd Hickam width 300 by height of 300"
         />
 
-        <h2 class="introduction mt-2" id="todd-hickam-psu-cs-undergraduate">
+        <h2 class="introduction text-start mt-2" id="todd-hickam-psu-cs-undergraduate">
           PSU Undergraduate
         </h2>
-        <ul className="">
+        <ul className="text-start">
           <li className="">PSU: Bachelor Computer Science 2024</li>
           <li className="">PCC: Associate Business Finance 2020</li>
         </ul>
-        <h2 class="introduction" id="seattle washington">
+        <h2 class="introduction text-start" id="seattle washington">
           Seattle, WA
         </h2>
-        <p class="introduction-body">
+        <p class="introduction-body text-start">
           At the age of 23 I opened my first business in North Seattle near the
           city of Lynwood, WA. This small automotive shop was off HWY99 that
           traversed North and South through Seattle, WA. For several years I
@@ -31,9 +30,8 @@ const Home = () => {
           difficult to expand my little company, I negotiated a sale and moved
           back to Portland, OR.
         </p>
-        {/* </article> */}
       </div>
-      {/* </span> */}
+        </article>
     </>
   );
 };
