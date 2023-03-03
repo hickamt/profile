425b6f2afc7bad679bc1ecd66468ee7da84ef672
Todd <thickam@pdx.edu>
2023-03-02 19:07:04 -0800

    updating styles for resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import pdf from "../assets/Todd_Hickam_Resume_2023.pdf";
+const pdfView = `${pdf}#view=fitH`;
 
 const Resume = () => {
   return (
@@ -9,9 +10,8 @@ const Resume = () => {
           <h1 className="resume-heading text-center">Resume</h1>
         <iframe
           className="iframe-resume"
-          src={pdf}
-          // width="90%"
-          height="750px"
+          src={pdfView}
+          height="725px"
           frameborder="0"></iframe>
         </div>
       </div>
