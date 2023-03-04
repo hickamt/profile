2daad753b64eee334aa78f328ef1ff047327a5ef
Todd <thickam@pdx.edu>
2023-03-04 11:49:51 -0800

    working on various home page cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,33 @@
 const Home = () => {
   return (
     <>
-    <article>
-      <div className="container profile-body justify-content-center">
-        <h1 className="">Todd Hickam</h1>
-        <img
-          className="profile-image my-2"
-          src="todd_hickam.jpg"
-          alt="headshot of Todd Hickam width 300 by height of 300"
-        />
-
-        <h2 className="introduction text-start mt-2" id="todd-hickam-psu-cs-undergraduate">
-          PSU Undergraduate
-        </h2>
-        <ul className="text-start">
-          <li className="">PSU: Bachelor Computer Science 2024</li>
-          <li className="">PCC: Associate Business Finance 2020</li>
-        </ul>
-        <h2 className="introduction text-start" id="seattle washington">
-          Seattle, WA
-        </h2>
-        <p className="introduction-body text-start">
-          At the age of 23 I opened my first business in North Seattle near the
-          city of Lynwood, WA. This small automotive shop was off HWY99 that
-          traversed North and South through Seattle, WA. For several years I
-          developed local relationships with car dealers and small businesses
-          while promoting retail sales through small adds and word of mouth.
-          Realizing the automotive saturation in the Seattle area would make it
-          difficult to expand my little company, I negotiated a sale and moved
-          back to Portland, OR.
-        </p>
+      <div className="container-md rounded-2 profile-body mt-4">
+        <div className="row mx-auto py-4">
+          <div className="col-lg-4 col-sm-12 profile-image">
+            <img
+              className="profile-image rounded-2"
+              src="../../public/img/profile/Todd_Hickam_Mercury.jpg"
+              alt="headshot of Todd Hickam width 300 by height of 300"
+            />
+          </div>
+          <div className="col-lg-4 col-sm-12 mt-2">
+            <h1 className="fs-2">Todd Hickam</h1>
+            <p className="introduction-body text-start">
+              Portland, Oregon Resident
+            </p>
+            <p className="introduction-body text-start">
+              Computer Science Undergrad
+            </p>
+            <p className="introduction-body text-start">Web Dev Passionate</p>
+            <p className="introduction-body text-start">
+              Knowledge Disseminater
+            </p>
+            <p className="introduction-body text-start">
+              Photo & Music Hobbyist
+            </p>
+          </div>
+        </div>
       </div>
-        </article>
     </>
   );
 };
---
+++ src/pages/Home_2.jsx
@@ -0,0 +1,11 @@
+import  tbird1  from "../../public/img/thunderbird/tbird1.jpg";
+
+const Home = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Home;
