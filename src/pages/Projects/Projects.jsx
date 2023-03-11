fe6b38db0ae433f81220a5004c3f78697a846792
Todd <thickam@pdx.edu>
2023-03-11 13:11:34 -0800

    mapping each card and properties from array object

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -34,84 +34,75 @@ const ProjectStyles = styled.div`
     }
   }
 `;
+const projects = [
+  {
+    id: 0,
+    imgSrc: folderImage,
+    title: "HttpOnly",
+    description:
+      "Ongoing project template with ReactJS & ExpressJS using HTTP Only Cookie",
+    buttonIMG: stackblitz,
+  },
+  {
+    id: 1,
+    imgSrc: folderImage,
+    cardIMG: folderImage,
+    title: "PSU Shell",
+    description:
+      "Custom shell exhibiting redirects, piping, and other standard exec() commands",
+    buttonIMG: github,
+  },
+  {
+    id: 2,
+    imgSrc: folderImage,
+    title: "Pthreads",
+    description:
+      "Find all primes to one-billion using multi-thread processing and mutex locks",
+    buttonIMG: github,
+  },
+];
 
+// implemented mapping across cards and formatting broke for row.
+// I had no idea if it was my implementation, an issue with mapping and bootstrap, or
+// if I had deleted a required div.className.
+// Resolution: reviewed GitHub revision history and saw that one of the divs had to be
+// mapped inside and was placed outside
 const Projects = () => {
   return (
     <>
       <ProjectStyles>
-        <h1 className="text-center mt-3" hidden>
-          Personal and Collegiate Programs
-        </h1>
-        <div className="container">
-          <div className="row mx-auto">
-            {/* Card One */}
-            <div className="col-md-4 col-sm-12">
-              <div className="card bg-dark">
-                <img src={folderImage} alt="" className="card-img-top" />
-                <div className="card-body bg-white text-dark text-center">
-                  <h2 className="card-title fs-3 text-center">HttpOnly</h2>
-                  <p className="card-text">
-                    Ongoing project template with ReactJS & ExpressJS using HTTP
-                    Only Cookie
-                  </p>
-                  <a
-                    href="https://stackblitz.com/github/hickamt/HTTP_Only_App_Template"
-                    target="_blank"
-                    className="project1">
-                    <img
-                      className="custom-button"
-                      src={stackblitz}
-                      alt="image of stackblitz button"
-                    />
-                  </a>
+      <h1 className="text-center mt-3" hidden>
+        Personal and Collegiate Programs
+      </h1>
+      <div className="container">
+        <div className="row mx-auto">
+          {projects.map((project) => {
+            return (
+              <div className="col-md-4 col-sm-12">
+                <div key={project.id} className="card bg-dark">
+                  <img src={project.imgSrc} alt="" className="card-img-top" />
+                  <div className="card-body bg-white text-dark text-center">
+                    <h2 className="card-title fs-3 text-center">
+                      {project.title}
+                    </h2>
+                    <p className="card-text">{project.description}</p>
+                    <a
+                      href="https://stackblitz.com/github/hickamt/HTTP_Only_App_Template"
+                      target="_blank"
+                      className="project1">
+                      <img
+                        className="custom-button"
+                        src={project.buttonIMG}
+                        alt="image of stackblitz button"
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            {/* Card Two */}
-            <div className="col-md-4 col-sm-12">
-              <div className="card bg-dark">
-                <img src={folderImage} alt="" className="card-img-top" />
-                <div className="card-body bg-white text-dark text-center">
-                  <h2 className="card-title fs-3 text-center">PSU Shell</h2>
-                  <p className="card-text">
-                    Custom shell exhibiting redirects, piping, and other
-                    standard exec() commands.
-                  </p>
-                  <a
-                    href="https://github.com/hickamt/PSU-Shell"
-                    target="_blank"
-                    className="project2 shell">
-                    <img
-                      className="custom-button"
-                      src={github}
-                      alt="image of psu logo"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            {/* Card Three */}
-            <div className="col-md-4 col-sm-12">
-              <div className="card bg-dark">
-                <img src={folderImage} alt="" className="card-img-top" />
-                <div className="card-body bg-white text-dark text-center">
-                  <h2 className="card-title fs-3 text-center">Pthreads</h2>
-                  <p className="card-text">
-                    Find all primes to one-billion using multi-thread and mutex
-                    process
-                  </p>
-                  <img
-                    className="custom-button"
-                    src={github}
-                    alt="image of psu logo"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
+      </div>
       </ProjectStyles>
     </>
   );
