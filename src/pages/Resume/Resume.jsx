63699b1f9f2c26448d74eda274662db463a25d5f
Todd <thickam@pdx.edu>
2023-03-10 17:52:41 -0800

    leaving at resume wide/mobile creation

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,34 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import ResumeMobile from "./Views/ResumeMobile";
-import ResumePDF from "./Todd_Hickam_Resume_2023.pdf";
-const pdfView = `${ResumePDF}#view=fitH`;
-
-const ResumeIFrame = () => {
-  return (
-    <div className="row justify-content-evenly">
-      <h1 className="resume-heading text-center">Resume Wide Screen</h1>
-      <iframe
-        className="iframe-resume"
-        src={pdfView}
-        height="725px"
-        frameborder="0"></iframe>
-    </div>
-  );
-};
-
-// const ResumeMobile = () => {
-//   return (
-//     <div className="row justify-content-evenly">
-//       <h1 className="resume-heading text-center">Resume Mobile</h1>
-//       <iframe
-//         className="iframe-resume"
-//         src={pdfView}
-//         height="725px"
-//         frameborder="0"></iframe>
-//     </div>
-//   );
-// };
+import ResumeWideScreen from "./Views/ResumeWideScreen";
 
 const Resume = () => {
   const [windowSize, setWindowSize] = useState([
@@ -50,7 +23,7 @@ const Resume = () => {
   return (
     <>
       <div className="resume d-col justify-content-center">
-        {windowSize[0] < 760 ? <ResumeMobile /> : <ResumeIFrame />}
+        {windowSize[0] < 760 ? <ResumeMobile /> : <ResumeWideScreen />}
       </div>
     </>
   );
---
+++ src/pages/Resume/Views/ResumeWideScreen.jsx
@@ -0,0 +1,17 @@
+import ResumePDF from "./Todd_Hickam_Resume_2023.pdf";
+const pdfView = `${ResumePDF}#view=fitH`;
+
+const ResumeWideScreen = () => {
+  return (
+    <div className="row justify-content-evenly">
+      <h1 className="resume-heading text-center">Resume Wide Screen</h1>
+      <iframe
+        className="iframe-resume"
+        src={pdfView}
+        height="725px"
+        frameborder="0"></iframe>
+    </div>
+  );
+};
+
+export default ResumeWideScreen;
