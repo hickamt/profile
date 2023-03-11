ae4a21e64c3102e5aa35662ad38e30ac6c4197a5
Todd <thickam@pdx.edu>
2023-03-11 13:43:58 -0800

    abstracted card body data to separate directory and file

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import folderImage from "./white_folder.png";
-import github from "./github.png";
-import stackblitz from "./stackblitz.png";
+import { projects } from "./data/ProjectData";
+// import folderImage from "./white_folder.png";
+// import github from "./github.png";
+// import stackblitz from "./stackblitz.png";
 
 const ProjectStyles = styled.div`
   margin-top: 4em;
@@ -34,33 +35,6 @@ const ProjectStyles = styled.div`
     }
   }
 `;
-const projects = [
-  {
-    id: 0,
-    imgSrc: folderImage,
-    title: "HttpOnly",
-    description:
-      "Ongoing project template with ReactJS & ExpressJS using HTTP Only Cookie",
-    buttonIMG: stackblitz,
-  },
-  {
-    id: 1,
-    imgSrc: folderImage,
-    cardIMG: folderImage,
-    title: "PSU Shell",
-    description:
-      "Custom shell exhibiting redirects, piping, and other standard exec() commands",
-    buttonIMG: github,
-  },
-  {
-    id: 2,
-    imgSrc: folderImage,
-    title: "Pthreads",
-    description:
-      "Find all primes to one-billion using multi-thread processing and mutex locks",
-    buttonIMG: github,
-  },
-];
 
 // implemented mapping across cards and formatting broke for row.
 // I had no idea if it was my implementation, an issue with mapping and bootstrap, or
@@ -71,38 +45,38 @@ const Projects = () => {
   return (
     <>
       <ProjectStyles>
-      <h1 className="text-center mt-3" hidden>
-        Personal and Collegiate Programs
-      </h1>
-      <div className="container">
-        <div className="row mx-auto">
-          {projects.map((project) => {
-            return (
-              <div className="col-md-4 col-sm-12">
-                <div key={project.id} className="card bg-dark">
-                  <img src={project.imgSrc} alt="" className="card-img-top" />
-                  <div className="card-body bg-white text-dark text-center">
-                    <h2 className="card-title fs-3 text-center">
-                      {project.title}
-                    </h2>
-                    <p className="card-text">{project.description}</p>
-                    <a
-                      href="https://stackblitz.com/github/hickamt/HTTP_Only_App_Template"
-                      target="_blank"
-                      className="project1">
-                      <img
-                        className="custom-button"
-                        src={project.buttonIMG}
-                        alt="image of stackblitz button"
-                      />
-                    </a>
+        <h1 className="text-center mt-3" hidden>
+          Personal and Collegiate Programs
+        </h1>
+        <div className="container">
+          <div className="row mx-auto">
+            {projects.map((project) => {
+              return (
+                <div className="col-md-4 col-sm-12">
+                  <div key={project.id} className="card bg-dark">
+                    <img src={project.cardSrc} alt="" className="card-img-top" />
+                    <div className="card-body bg-white text-dark text-center">
+                      <h2 className="card-title fs-3 text-center">
+                        {project.title}
+                      </h2>
+                      <p className="card-text">{project.description}</p>
+                      <a
+                        href="https://stackblitz.com/github/hickamt/HTTP_Only_App_Template"
+                        target="_blank"
+                        className="project1">
+                        <img
+                          className="custom-button"
+                          src={project.buttonSrc}
+                          alt="image of stackblitz button"
+                        />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
       </ProjectStyles>
     </>
   );
---
+++ src/pages/Projects/data/ProjectData.jsx
@@ -0,0 +1,30 @@
+import folderIMG from "./white_folder.png"
+import github from "./github.png";
+import stackblitz from "./stackblitz.png";
+
+export const projects = [
+  {
+    id: 0,
+    cardSrc: folderIMG,
+    title: "HttpOnly",
+    description:
+      "Ongoing project template with ReactJS & ExpressJS using HTTP Only Cookie",
+    buttonSrc: stackblitz,
+  },
+  {
+    id: 1,
+    cardSrc: folderIMG,
+    title: "PSU Shell",
+    description:
+      "Custom shell exhibiting redirects, piping, and other standard exec() commands",
+    buttonSrc: github,
+  },
+  {
+    id: 2,
+    cardSrc: folderIMG,
+    title: "Pthreads",
+    description:
+      "Find all primes to one-billion using multi-thread processing and mutex locks",
+    buttonSrc: github,
+  },
+];
