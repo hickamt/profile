f7df9c50323495ccaa8367873c4ea3630872bd6e
Todd <thickam@pdx.edu>
2023-03-11 20:45:08 -0800

    updating data property naming conventions

--- src/pages/Projects/component/CardBody.jsx
+++ src/pages/Projects/component/CardBody.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CardBody = ({
   cardSrc,
   cardAlt,
-  href,
-  title,
+  cardTitle,
   description,
+  buttonHref,
   buttonSrc,
   buttonAlt,
 }) => {
@@ -14,9 +14,9 @@ const CardBody = ({
       <div className="card bg-dark">
         <img src={cardSrc} alt={cardAlt} className="card-img-top" />
         <div className="body bg-white text-dark text-center">
-          <h2 className="title fs-3 text-center">{title}</h2>
+          <h2 className="title fs-3 text-center">{cardTitle}</h2>
           <p className="text">{description}</p>
-          <a href={href} target="_blank" className="">
+          <a href={buttonHref} target="_blank" className="">
             <img className="custom-button" src={buttonSrc} alt={buttonAlt} />
           </a>
         </div>
